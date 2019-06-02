85f8dbe9c4d92428ea1c7db355733f6980185c7e
Carlos Fontes <fonstack@gmail.com>
2019-06-02 01:53:33 -0400

    finalizado código de sub y pub

--- pub.py
+++ pub.py
@@ -7,30 +7,54 @@ import paho.mqtt.client
 import paho.mqtt.publish
 import numpy as np
 import datetime
+import math
+import psycopg2
 
 
 def on_connect(client, userdata, flags, rc):
     print('Publicando')
 
+
 def main():
+    conn = psycopg2.connect(host='bydl8yhl1xvycflftlju-postgresql.services.clever-cloud.com', user='ujegvu5yu2q28i5dthj9', password='86WWh0iQz4n8dfN82WGW', dbname='bydl8yhl1xvycflftlju')
+    cursor = conn.cursor()
+    conn.autocommit = True
+  
     client = paho.mqtt.client.Client("Pipo-subs", False)
     client.qos = 0
     client.connect(host='localhost')
-    info =100
-    horaAct= datetime.datetime.now().replace(minute=0, second=0)
-    while(info>0):
-        hora = horaAct + datetime.timedelta(minutes=np.random.uniform(0,60))
-        cam = int(np.random.uniform(1,3))
-        payload = {
-            "id_cam": str(cam),
-            "fecha": str(hora)
-        }
-        client.publish('local/puerta',json.dumps(payload),qos=0)
-        print(payload)
-        time.sleep(1)
-        info = info -1
+    
+    while(True):
+        random = int(np.random.uniform(0, 2))
+        print(random)
+        if (random == 1):
+            sexo = 'masculino'
+        else:
+            sexo = 'femenino'
+            
+        edad = int(np.random.uniform(10, 60))
+        hora_entrada = time.strftime("%d/%m/%y %H:%M:%S")
+        id_camara = int(np.random.uniform(1, 4))
+        id_persona = int(np.random.uniform(1000000, 50000000))
         
+        payload = {
+            "id_camara": id_camara,
+            "hora_entrada": hora_entrada,
+            "hora_salida": None,
+            "dentro_tienda": False,
+            "id_persona": id_persona
+        }
+        
+        query = "INSERT INTO persona (id, sexo, edad) VALUES (%s, %s, %s)";
+        cursor.execute(query, (id_persona, sexo, edad))
+        conn.commit()
+        
+        client.publish('sambil/accesos', json.dumps(payload), qos=0)
+          
+        time.sleep(np.random.uniform(2, 4))
+
+
 if __name__ == '__main__':
     main()
-    
+
 sys.exit(0)
--- sub.py
+++ sub.py
@@ -2,40 +2,50 @@ import ssl
 import sys
 import json
 import paho.mqtt.client
-import psycopg2 
+import psycopg2
 
 
-conn = psycopg2.connect(host = 'localhost', user= 'postgres', password ='password', dbname= 'testDB')
+conn = psycopg2.connect(host='bydl8yhl1xvycflftlju-postgresql.services.clever-cloud.com',
+                        user='ujegvu5yu2q28i5dthj9', password='86WWh0iQz4n8dfN82WGW', dbname='bydl8yhl1xvycflftlju')
+
 
 def doQuery(a):
     a = a.decode('utf-8').replace("'", '"')
-    print(a)
     a = json.loads(a)
-    var1 = a['id_cam']
-    var2 = a['fecha']
+    
+    id_camara = a['id_camara']
+    hora_entrada = a['hora_entrada']
+    hora_salida = a['hora_salida']
+    dentro_tienda = a['dentro_tienda']
+    id_persona = a['id_persona']
+
     cur = conn.cursor()
-    sql = "INSERT INTO stat (id_cam, fecha) VALUES (%s, %s);"
-    cur.execute(sql, (var1, var2))
+    sql = "INSERT INTO estadistica_camara (id_camara, hora_entrada, hora_salida, id_persona, dentro_tienda, rip) VALUES (%s, %s, %s, %s, %s, %s);"
+    cur.execute(sql, (id_camara, hora_entrada, hora_salida, id_persona, dentro_tienda, False))
     conn.commit()
 
-def on_connect(client, userdata, flags, rc):
-    print("Conectao' (%s)" % client._client_id)
-    client.subscribe(topic='local/#', qos = 0) 
 
-def on_message(client, userdata, message): 
+def on_connect(client, userdata, flags, rc):
+    print("Conectado! (%s)" % client._client_id)
+    client.subscribe(topic='sambil/#', qos=0)
+
+
+def on_message(client, userdata, message):
+    print('------------------------------')
     print('topic: %s' % message.topic)
     print('payload: %s' % message.payload)
     print('------------------------------')
     doQuery(message.payload)
+
     
-     
 
 def main():
     client = paho.mqtt.client.Client()
     client.on_connect = on_connect
-    client.message_callback_add('local/puerta', on_message)
+    client.message_callback_add('sambil/accesos', on_message)
     client.connect(host='localhost')
     client.loop_forever()
+
 
 if __name__ == '__main__':
     main()
