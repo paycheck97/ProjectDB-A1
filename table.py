8a21e00377e1ff9e71287a24ed851dac39eb0244
paycheck97 <43653779+paycheck97@users.noreply.github.com>
2019-06-08 19:15:01 -0400

    views & chart

--- table.py
+++ table.py
@@ -1,15 +1,50 @@
-import psycopg2 
+import matplotlib
+import matplotlib.pyplot as plt
+import psycopg2
 import pandas as pd
 
-conn = psycopg2.connect(host = 'localhost', user= 'postgres', password ='password', dbname= 'testDB')
+conn = psycopg2.connect(host='bydl8yhl1xvycflftlju-postgresql.services.clever-cloud.com',
+                        user='ujegvu5yu2q28i5dthj9', password='86WWh0iQz4n8dfN82WGW', dbname='bydl8yhl1xvycflftlju')
 
+cur = conn.cursor()
+sql = "SELECT COUNT(id) FROM venta WHERE id_persona IS NOT null;"
+cur.execute(sql)
+conTel = cur.fetchall()
+conTel = conTel[0][0]
+
+cur = conn.cursor()
+sql = "SELECT COUNT(id) FROM venta WHERE id_persona IS null;"
+cur.execute(sql)
+sinTel = cur.fetchall()
+sinTel = sinTel[0][0]
+
+total = conTel + sinTel
+
+
+names = ['Grupo con Telefono', 'Grupo sin Telefono']
+values = [(conTel/2)*100, (sinTel/2)*100]
+
+plt.figure(figsize=(15, 5))
+
+plt.subplot(131)
+plt.bar(names, values)
+plt.grid(True)
+plt.ylabel('Valores Porcentuales')
+plt.suptitle('Ventas (porcentaje)')
+plt.axis([-0.5, 1.5, 0 , 101])
+plt.show  #grafico comparativa de compras con y sin telefono
+
+#Tabla en una celda
 sql = '''
-    SELECT stat.fecha, camara.id, acceso.nombre_acceso, centro_comercial.nombre
-    FROM stat
-    INNER JOIN camara ON stat.id_cam = camara.id
-    INNER JOIN acceso ON camara.id_acc = acceso.id
-    INNER JOIN centro_comercial 
-    ON acceso.id_cc = centro_comercial.id;
+    SELECT * FROM numero_accesos
+'''
+
+df = pd.read_sql(sql, conn)
+
+#En otra celda
+df
+sql = '''
+    SELECT * FROM mas_visitas;
 '''
 
 df = pd.read_sql(sql, conn)
